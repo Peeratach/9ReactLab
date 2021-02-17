a96fb64d5943e9e0debc00d945bd9008194ab648
Peeratach <peeopeeopee@gmail.com>
2021-02-17 12:43:56 +0700

    LabReact

---
+++ src/items.js
@@ -0,0 +1,26 @@
+const items =[
+    {
+      title :"ภาคิน",
+      thumbnaiUrl :"/1.jpg"
+      
+    
+    },
+    {
+      title :"โคมไฟสวยดี",
+      thumbnaiUrl :"/2.jpg"
+    },
+    {
+      title :"จารแดง",
+      thumbnaiUrl :"/3.jpg"
+      
+    
+    },
+    {
+      title :"วงดนตรีมั้ง?",
+      thumbnaiUrl :"/4.jpg"
+      
+    
+    }
+  ];
+export default items;
+
